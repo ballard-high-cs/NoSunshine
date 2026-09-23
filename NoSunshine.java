0efd4b19216f64fe7b0b15f696b12b71fe271af7
GavinOnGithub <162376944+GavinOnGithub@users.noreply.github.com>
2026-09-23 11:36:06 -0700

    Create NoSunshine.java

---
+++ NoSunshine.java
@@ -0,0 +1,52 @@
+public class NoSunshine {
+   public static void main(String[] ags) {
+      v(1);
+      System.out.println("It's not warm when she's away");
+      v(1);
+      System.out.println("And she's gone too long");
+      v(3);
+      v(2);
+      System.out.println("Wonder this time where she's gone");
+      System.out.println("Wonder if she's gone to stay");
+      v(1);
+      v(4);
+      v(3);
+      v(2);
+      System.out.println("And I know, I know, I know, i know");
+      v(6);
+      v(6);
+      v(6);
+      v(6);
+      System.out.println("I know, I know");
+      v(2);
+      System.out.println("Hey, I ought to leave young thing alone");
+      System.out.println("But ain't no sunshine when she's gone, whoa-whoa");
+      v(1);
+      System.out.println("Only darkness every day");
+      v(2);
+      v(1);
+      System.out.println("And this house jus ain't no home");
+      v(2);
+      v(3);
+      v(3);
+      v(3);
+      v(3); 
+  } 
+  public static void v(int a) {
+      if (a == 1) {
+         System.out.println("Ain't no sunshine when she's gone");
+      }
+      if (a == 2) {
+         System.out.println();
+      }   
+      if (a == 3) {
+         System.out.println("Anytime she goes away");
+      }   
+      if (a == 4) {
+         System.out.println("And this house just ain't no home");
+      }
+      if (a == 6) {
+         System.out.println("I know, I know, I know, I know, I know");
+      }   
+  }
+}
