def4925966ad71066eac1c0c23a988c093f7c2ee
GavinOnGithub <162376944+GavinOnGithub@users.noreply.github.com>
2026-09-23 11:39:39 -0700

    Update NoSunshine.java

--- NoSunshine.java
+++ NoSunshine.java
@@ -1,5 +1,5 @@
 public class NoSunshine {
-   public static void main(String[] ags) {
+   public static void main(String[] ags){
       v(1);
       System.out.println("It's not warm when she's away");
       v(1);
@@ -32,20 +32,20 @@ public class NoSunshine {
       v(3);
       v(3); 
   }
-  public static void v(int a) {
-      if (a==1) {
+  public static void v(int a){
+      if (a==1){
          System.out.println("Ain't no sunshine when she's gone");
       }
-      if (a==2) {
+      if (a==2){
          System.out.println();
       }   
-      if (a==3) {
+      if (a==3){
          System.out.println("Anytime she goes away");
       }   
-      if (a==4) {
+      if (a==4){
          System.out.println("And this house just ain't no home");
       }
-      if (a==6) {
+      if (a==6){
          System.out.println("I know, I know, I know, I know, I know");
       }   
   }
