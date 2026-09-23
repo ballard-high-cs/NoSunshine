9b5abd531aa4ccca323d5374af34649c94ec7d17
GavinOnGithub <162376944+GavinOnGithub@users.noreply.github.com>
2026-09-23 11:48:47 -0700

    Update NoSunshine.java

--- NoSunshine.java
+++ NoSunshine.java
@@ -30,23 +30,17 @@ public class NoSunshine{
       v(3);
       v(3);
       v(3);
-      v(3); 
-  }
+      v(3); }
   public static void v(int a){
       if (a==1){
-         System.out.println("Ain't no sunshine when she's gone");
-      }
+         System.out.println("Ain't no sunshine when she's gone");}
       if (a==2){
-         System.out.println();
-      }   
+         System.out.println();}   
       if (a==3){
-         System.out.println("Anytime she goes away");
-      }   
+         System.out.println("Anytime she goes away");}   
       if (a==4){
-         System.out.println("And this house just ain't no home");
-      }
+         System.out.println("And this house just ain't no home");}
       if (a==6){
-         System.out.println("I know, I know, I know, I know, I know");
-      }   
+         System.out.println("I know, I know, I know, I know, I know");}   
   }
 }
